72dc8621e6f76cb6328eb0c39c886505e32baa08
José Alfonso Rosas Caamaño <j.alfonsorc@gmail.com>
2022-10-17 20:40:37 -0500

    segundo commit

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -4,7 +4,7 @@ export const GifGridItem = ({id, title, imagen}) => {
 	console.log(id, title, imagen);
 
 	return (
-		<div className="card">
+		<div className="card animate__animated animate__bounce ">
 			<img src={imagen} alt={title} />
 			<p>{title}</p>
 		</div>
---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,20 @@
+import {useState, useEffect} from "react";
+import {getGifs} from "../helpers/getGifs";
+
+export const useFetchGifs = (category) => {
+	const [state, setState] = useState({
+		data: [],
+		loading: true,
+	});
+
+	useEffect(() => {
+		getGifs(category).then((imgs) => {
+			setState({
+				data: imgs,
+				loading: false,
+			});
+		});
+	}, [category]); //quiero que dispare la petición si la categoria cambia. */
+
+	return state; //{data:[], loading:true}
+};
